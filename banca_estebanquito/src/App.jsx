4ea592b836a6f321ad8faa8496eb6857e5c08902
anthony gonzalez <agonzalezv@correo.iue.edu.co>
2025-10-20 10:19:21 -0500

    cambio

--- banca_estebanquito/src/App.jsx
+++ banca_estebanquito/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Login from './components/login/Login'
 import RegistroUsuarios from './components/registro_usuarios/RegistroUsuarios'
 import ReportesFinancieros from './components/reportes_financieros/ReportesFinancieros'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 
 import Transacciones from './components/Transacciones/Transacciones'
 import Prestamos from './components/Prestamos/Prestamos'
