feb6d78c53753da830e79d2164b2f062e4444927
Angelito-148 <maperezm@correo.iue.edu.co>
2025-10-20 10:26:49 -0500

    fronted gestion cuentas

--- banca_estebanquito/src/App.jsx
+++ banca_estebanquito/src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AccountManagement from './components/GestionCuentas/AccountManagement';
 import React from 'react';
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AccountManagement />} />
+      </Routes>
+    </Router>
   )
 }
 export default App
